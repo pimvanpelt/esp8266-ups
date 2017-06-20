b7d8cbd5ac01e6fb8ae554e8a80e0cf96a514065
Pim van Pelt <pim@ipng.nl>
2017-06-20 09:39:40 -0700

    Fold p012.{cpp,h} into adcunit.{cpp,h}

--- src/adcunit.cpp
+++ src/adcunit.cpp
@@ -1,6 +1,118 @@
 #include "config.h"
 #include "adcunit.h"
 
+// ==============================================================
+// I2C PRIVATE
+// ==============================================================
+// ==============================================================
+// **************************************************************
+// gets an unsigned 16 bit value from the i2c bus
+// **************************************************************
+uint16_t ADCIC::i2_16bit()
+{
+  uint16_t rv;
+  Wire.beginTransmission(_i2c_address);
+  Wire.requestFrom((uint8_t) _i2c_address, (uint8_t) 2); // returns 2 bytes
+  rv=Wire.read()*256; // high byte
+  rv+=Wire.read(); // low byte
+  Wire.endTransmission();
+  return rv;
+}
+
+// =============================================================================
+// I2C ADC P011/2
+// =============================================================================
+// =============================================================================
+// *****************************************************************************
+// constructors
+// *****************************************************************************
+ADCIC::ADCIC(uint8_t i2c_addr, uint8_t reference)
+{
+  _i2c_address=i2c_addr;  
+  Wire.begin(); // join i2c bus
+  ADCIC::set_vref(reference);
+}
+
+
+// *****************************************************************************
+// Get value from channel
+// *****************************************************************************
+uint16_t ADCIC::get(uint8_t channel)
+{
+    Wire.beginTransmission(_i2c_address);
+    Wire.write(1);
+    Wire.write(channel);
+    Wire.endTransmission();
+    return i2_16bit();
+}
+
+// *****************************************************************************
+// Get version
+// *****************************************************************************
+uint16_t ADCIC::get_version()
+{
+    Wire.beginTransmission(_i2c_address);
+    Wire.write(0xA0);
+    Wire.endTransmission();
+    return i2_16bit();
+}
+
+// *****************************************************************************
+// Digital output, set either high or low, channel is a value from 0 to 3
+// *****************************************************************************
+void ADCIC::digital(uint8_t channel, char op)
+{
+    if(channel > 3) channel = 3;
+    Wire.beginTransmission(_i2c_address);
+    Wire.write(2);
+    Wire.write(channel);
+    Wire.write(op);
+    Wire.endTransmission();
+}
+
+// *****************************************************************************
+// Set ADCIC reference voltage
+// Use ADCIC_* defines from the header file.
+// *****************************************************************************
+void ADCIC::set_vref(uint8_t reference)
+{
+    if(reference > ADCIC_VDD) reference = ADCIC_VDD;
+    Wire.beginTransmission(_i2c_address);
+    Wire.write(3);
+    Wire.write(reference);
+    Wire.endTransmission();
+}
+
+void ADCIC::eeprom_write(uint8_t loc, uint8_t val)
+{
+  if (loc == 0)
+    return;
+
+  // 32<=loc<=255 are user defined bytes
+  Wire.beginTransmission(_i2c_address);
+  Wire.write(0x91);
+  Wire.write(loc);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+uint8_t ADCIC::eeprom_read(uint8_t loc)
+{
+  uint8_t ret;
+
+  Wire.beginTransmission(_i2c_address);
+  Wire.write(0x90);
+  Wire.write(loc);
+  Wire.write(1);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(_i2c_address);
+  Wire.requestFrom((uint8_t) _i2c_address, (uint8_t) 1);
+  ret=Wire.read();
+  Wire.endTransmission();
+  return ret;
+}
+
 ADCUnit::ADCUnit(uint8_t i2c_addr)
 {
   adc = new ADCIC(i2c_addr, ADCIC_VREF);
@@ -76,7 +188,6 @@ uint16_t ADCUnit::getVersion()
 {
   return version;
 }
-
 
 /* First time setup 
  * Set ADC EEPROM values:
--- src/adcunit.h
+++ src/adcunit.h
@@ -1,8 +1,76 @@
 #ifndef _ADCUNIT_H
 #define _ADCUNIT_H
+/*
+ * Based on ADC I2C - Library for ByVac P017 www.byvac.com
+ * Copyright (c) 2013 Jim Spence.  All right reserved.
+ * www.byvac.com - see terms and conditions for using hardware
+ * 
+ * Datasheet: http://www.byvac.com/downloads/datasheets/P011%20DataSheet.pdf
+ * Adapted by Pim van Pelt <pim@ipng.nl>
+*/
 
-#include "p012.h"
 #include "RunningAverage.h"
+#include "Arduino.h"
+#include "Wire.h"
+
+#define ADCIC_VREF 0
+#define ADCIC_1V   1
+#define ADCIC_2V   2
+#define ADCIC_4V   3
+#define ADCIC_VDD  4
+
+class ADCIC : public TwoWire
+{
+    public:
+        /* Initialize the ADC -- there's really nothing to do, so all this does
+         *  is store the i2c_addr. You can alternatively initialize the voltage
+         *  reference here (see 'set_vref' method below for details)
+         *  Safe defaults are: i2c_addr = 0x34; reference = ADCIC_VDD
+         */
+        ADCIC(uint8_t i2c_addr, uint8_t reference);
+
+        /* Return reading from channel. Channel can be between 1 and 8
+         * for ADC readings, or 9 for an internal thermometer. Returned
+         * value is between 0..1023 (ie a 10-bit reading) 
+         */
+        uint16_t get(uint8_t channel);
+
+        /* Returned a 16 bit integer with the I2C ADC version number
+         * Most chips in the field return 0x0107 (ie version 1.7) 
+         */
+        uint16_t get_version();
+
+        /* Sets digital output channel to HIGH or LOW
+         * channel must be between 1 and 4
+         */
+        void digital(uint8_t channel, char op);
+
+        /* Sets the Voltage Reference for the ADC to one of:
+         *  ADCIC_VREF (0): Use the VREF pin
+         *  ADCIC_1V   (1): Use 1.024 Volts
+         *  ADCIC_2V   (2): Use 2.048 Volts
+         *  ADCIC_4V   (3): Use 4.096 Volts
+         *  ADCIC_VDD  (4): Use VDD pin
+         *  Any other value of reference will mean "ADCIC_VDD"
+         */
+        void set_vref(uint8_t reference);
+
+        /* Sets scan time in increments of 128uS
+         * Valid values are [1,254] which is 128uS .. 32.5mS
+         * Factory default is '8' which is 1024uS or 1ms
+         */
+        void set_scanrate(uint8_t n);
+
+        /* Write EEPROM value at location */
+        void eeprom_write(uint8_t loc, uint8_t val);
+
+        /* Read EEPROM value at location */
+        uint8_t eeprom_read(uint8_t loc);
+
+    private:
+        uint8_t _i2c_address;
+        uint16_t i2_16bit();
+};
 
 class ADCUnit {
   public:
